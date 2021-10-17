b829c0966a7977970cad186bee12c3bde192be26
landry <1197581424@qq.com>
2021-10-17 19:00:00 +0300

    added and removed some things

--- myfolder/__init__.py
+++ myfolder/__init__.py
@@ -1,2 +1,7 @@
 def subtraction(a,b):
     return a-b
+
+
+def union_probability(pa,pb,p_intersection):
+    '''Compute the probability of P(A or B) given P(A),P(B), P(A and B)'''
+    return pa + pb - p_intersection
